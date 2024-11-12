d8b5161c9749daca4141223973dc720d9741cc6d
Leuta Taras <81424893+tar1kkk@users.noreply.github.com>
2024-11-12 10:31:18 +0200

    add other files

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,24 @@
-import NextAuth, {AuthOptions} from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import { compare, hashSync } from 'bcrypt';
+import { UserRole } from '@prisma/client';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
 import {prisma} from "@/prisma/prisma-client";
-import {compare, hashSync} from "bcrypt";
-import {UserRole} from "@prisma/client";
 
-export const authOptions: AuthOptions = {
+export const authOptions: NextAuthOptions = {
+    session: {
+        strategy: 'jwt',
+    },
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID || '',
+            clientSecret: process.env.GITHUB_SECRET || '',
             profile(profile) {
                 return {
                     id: profile.id,
@@ -21,28 +29,30 @@ export const authOptions: AuthOptions = {
                 };
             },
         }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_SECRET_ID,
-        }),
         CredentialsProvider({
-            name: "Credentials",
+            name: 'Credentials',
             credentials: {
-                email: {label: "Email", type: "text"},
-                password: {label: "Password", type: "password"},
+                email: { label: 'E-Mail', type: 'text', placeholder: 'user@test.ru' },
+                password: { label: 'Пароль', type: 'password' },
             },
             async authorize(credentials) {
-                if (!credentials) {
-                    return null;
-                }
+                if (!credentials) return null;
+
+                const values = {
+                    email: credentials.email,
+                };
+
                 const findUser = await prisma.user.findFirst({
-                    where: {email: credentials.email},
+                    where: values,
                 });
 
                 if (!findUser) return null;
 
                 const isPasswordValid = await compare(credentials.password, findUser.password);
-                if (!isPasswordValid || !findUser.verified) return null;
+
+                if (!isPasswordValid) return null;
+
+                if (!findUser.verified) return null;
 
                 return {
                     id: String(findUser.id),
@@ -53,66 +63,80 @@ export const authOptions: AuthOptions = {
             },
         }),
     ],
-    secret: process.env.NEXTAUTH_SECRET,
-    session: {
-        strategy: 'jwt',
-    },
     callbacks: {
-        async signIn({user, account}) {
-            if (!user?.email) return false;
+        async signIn({ user, account }) {
             try {
+                if (account?.provider === 'credentials') {
+                    return true;
+                }
+
+                console.log(user, account);
+
+                if (!user.email) {
+                    return false;
+                }
+
                 const findUser = await prisma.user.findFirst({
                     where: {
                         OR: [
-                            {provider: account?.provider, providerId: account?.providerAccountId},
-                            {email: user.email},
+                            { provider: account?.provider, providerId: account?.providerAccountId },
+                            { email: user.email },
                         ],
                     },
                 });
 
                 if (findUser) {
                     await prisma.user.update({
-                        where: {id: findUser.id},
+                        where: {
+                            id: findUser.id,
+                        },
                         data: {
                             provider: account?.provider,
                             providerId: account?.providerAccountId,
                         },
                     });
-                } else {
-                    await prisma.user.create({
-                        data: {
-                            email: user.email,
-                            fullName: user.name || `User #${user.id}`,
-                            password: hashSync(user.id.toString(), 10),
-                            verified: account?.provider,
-                            providerId: account?.providerAccountId,
-                        },
-                    });
+                    return true;
                 }
+
+                await prisma.user.create({
+                    data: {
+                        email: user.email,
+                        fullName: user.name || 'User #' + user.id,
+                        password: hashSync(user.id.toString(), 10),
+                        verified: new Date(),
+                        provider: account?.provider,
+                        providerId: account?.providerAccountId,
+                    },
+                });
+
                 return true;
-            } catch (e) {
-                console.error("SignIn Error: ", e);
+            } catch (error) {
+                console.log(error);
                 return false;
             }
         },
-        async jwt({token}) {
+        async jwt({ token }) {
             const findUser = await prisma.user.findFirst({
-                where: {email: token.email},
+                where: {
+                    email: token.email!,
+                },
             });
 
             if (findUser) {
-                token.id = findUser.id;
+                token.id = String(findUser.id);
                 token.email = findUser.email;
                 token.fullName = findUser.fullName;
                 token.role = findUser.role;
             }
+
             return token;
         },
-        async session({session, token}) {
+        session({ session, token }) {
             if (session?.user) {
                 session.user.id = token.id;
                 session.user.role = token.role;
             }
+
             return session;
         },
     },
@@ -120,4 +144,4 @@ export const authOptions: AuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
